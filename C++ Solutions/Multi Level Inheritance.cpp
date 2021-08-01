9009ae4b55a08e9071f7844a152312c8a0d32392
gunjan-g <gunjang1202@gmail.com>
2021-08-02 01:31:51 +0530

    adding files

---
+++ C++ Solutions/Multi Level Inheritance.cpp
@@ -0,0 +1,64 @@
+/*
+
+Program Link:
+https://www.hackerrank.com/challenges/multi-level-inheritance-cpp/problem
+
+Ques.
+In this challenge, we explore multi-level inheritance. Suppose, we have a class A which 
+is the base class and we have a class B which is derived from class A and we have a 
+class C which is derived from class B, we can access the functions of both class A and 
+class B by creating an object for class C. Hence, this mechanism is called multi-level 
+inheritance. (B inherits A and C inherits B.)
+
+Create a class called Equilateral which inherits from Isosceles and should have a function 
+such that the output is as given below.
+
+Sample Output:
+I am an equilateral triangle
+I am an isosceles triangle
+I am a triangle
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Triangle{
+	public:
+		void triangle(){
+			cout<<"I am a triangle\n";
+		}
+};
+
+class Isosceles : public Triangle{
+  	public:
+  		void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+  		}
+};
+
+//Write your code here.
+
+class Equilateral : public Isosceles{
+    public:
+       void equilateral(){
+           cout<<"I am an equilateral triangle\n";
+       }
+};
+
+/* class Isosceles inherits class Triangle publically and class Equilateral inherits class
+ Isosceles publically. Thus, the object of class Equilateral can access the public members 
+ of class Isosceles and class Triangle */ 
+
+int main(){
+  
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
+}
