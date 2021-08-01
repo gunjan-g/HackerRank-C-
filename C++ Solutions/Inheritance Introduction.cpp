d2d81a8a977e3f0baf54463c0c3af49bf79b8fd1
gunjan-g <gunjang1202@gmail.com>
2021-08-02 01:26:59 +0530

    adding files

---
+++ C++ Solutions/Inheritance Introduction.cpp
@@ -0,0 +1,91 @@
+/*
+
+Program Link:
+https://www.hackerrank.com/challenges/inheritance-introduction/problem
+
+Theory:
+One of the important topics of Object Oriented Programming is Inheritance. Inheritance 
+allows us to define a class in terms of another class, which allows us in the reusability 
+of the code.Check out the code below:
+
+class Triangle{
+    public:
+        void triangle(){
+            cout<<"I am a triangle\n";
+        }
+};
+
+The class Triangle has a function called triangle(). Now we create a class derived from 
+the base class Triangle called Isosceles.
+
+class Isosceles : public Triangle{
+    public:
+        void isosceles(){
+            cout<<"I am an isosceles triangle\n";
+        }
+};
+
+Now we can create a derived class object and use it to access the functions of the base 
+class.
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+    isc.triangle();
+    return 0;
+}
+
+This code will print:
+I am an isosceles triangle
+I am a triangle
+
+Now write a function in Isosceles class such that the output is as given below.
+
+Sample Output:
+I am an isosceles triangle
+In an isosceles triangle two sides are equal
+I am a triangle
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Triangle{
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
+};
+
+class Description{
+    public:
+       void description(){
+           cout<<"In an isosceles triangle two sides are equal\n";
+       }
+};
+
+//Write your code here.
+
+//Isosceles class inherit publically from class Triangle and Description
+class Isosceles : public Triangle, public Description{
+    public:
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+    	}
+};
+
+/*the object of Isosceles class can access the public members of Triangle and Description
+class as class Isosceles publically inherit class Triangle and Description */ 	
+
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+  	isc.description();
+    isc.triangle();
+    return 0;
+}
